b9440c1f4a4b269e4b6c85037baae5f7edb9e1e2
frankemon <70698241+frankemon@users.noreply.github.com>
2020-11-11 18:36:34 +0100

    new keyboard

--- app.js
+++ app.js
@@ -3,30 +3,46 @@ const wordBank = [
 ];
 
 const alphabet =
-    'abcdefghijklmnopqrstuvwxyz'.split('');
+    'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
 console.log(alphabet); //can remove
 
+// const imgs = ['img/hangman-0.svg', 'img/hangman-1.svg', 'img/hangman-2.svg', 'img/hangman-3.svg', 'img/hangman-4.svg', 'img/hangman-5.svg'];
+
+let guesses = 0;
+let guessesLeft = 6;
+let hangmanImg;
 let blanks = [];
-let guess;
-let storedGuesses;
-let correctCounter;
 
-//create alphabet ul
 
-let buttons = function () {
-    myButtons = document.getElementById('keyboard');
-    letters = document.createElement('ul');
+// let newGameButtonEl = document.querySelector('#new-game-button');
+
+// newGameButtonEl = addEventListener('click', startGame);
+
+
+
+// create alphabet as buttons MAYBE REWRITE TO NORMAL FUNCTION
+function generateKeyboard() {
     for (let index = 0; index < alphabet.length; index++) {
-        letters.id = 'alphabet';
-        list = document.createElement('li');
-        list.id = 'letter';
-        list.innerHTML = alphabet[index];
-        // check();  ?????
-        myButtons.appendChild(letters);
-        letters.appendChild(list);
+        let keyboard = document.createElement('button');
+        keyboard.innerHTML = alphabet[index];
+        keyboard.id = alphabet[index];
+        keyboard.value = alphabet[index];
+        keyboard.onclick = function () {
+            getGuess(alphabet[index]);
+        }
+
+        document.querySelector('.keyboardtwo').appendChild(keyboard);
+
     }
 }
-buttons();
+
+function getGuess(letter) {
+    console.log(letter);
+    let clickedButton = document.querySelector('#' + letter);
+    console.log('getGuess: ' + clickedButton);
+    // clickedButton.disabled = true;
+}
+getGuess();
 
 // Pick a random word from wordBank
 function wordPicker() {
@@ -39,38 +55,61 @@ function wordPicker() {
 }
 
 
-console.log(wordBank); //can remove
+// console.log(wordBank); //can remove
 
 // Replace generatedWord with underscores
 function blankSpaces(string) {
     for (let index = 0; index < string.length; index++) {
         blanks.push('_');
-        console.log('test' + ' ' + index);
-        console.log(blanks)
+        // console.log('test' + ' ' + index);
+        // console.log(blanks)
 
     }
-    updateDOM();
+    updateDOM(); // ??????????????
 }
 
 function updateDOM() {
     document.querySelector('#hidden-word').textContent = blanks.join(' ');
 }
-// function createKeyboard(letter) {
-// let 
-// let button = document.createElement("keyboard-button");
-// button = document.createElement('button');
-// button.innerHTML = letter.toUpperCase();
-// button.id = "button-" + letter;
 
-// };
-// for (var i = 0; i < alphabet.length; i++) {
-//     var buttonElement = createKeyboard(alphabet[i]);
-//     document.getElementById("#keyboard").appendChild(buttonElement);
-// }
+// function that disables used buttons
+function disableButton() {
+    // let disabledButton = document.getElementById("keyboard");
+    // disabledButton.classList.add('disabled');
+    // document.getElementbyId("kbButton").addEventListener("click", disableButton);
+    ///////////////////////////////////////////////////////
+    // disableButton();  // move this out 
+}
 
-// createKeyboard();
 
+///////////////////////////////////////
+// Initiate game as new game button
+///////////////////////////////////////
+function startGame() {
+
+
+}
+generateKeyboard();
 const answer = wordPicker();
-
 blankSpaces(answer);
+// keyboardButtons();
+
+
+
+
+
+    // function createKeyboard(letter) {
+    // let 
+    // let button = document.createElement("keyboard-button");
+    // button = document.createElement('button');
+    // button.innerHTML = letter.toUpperCase();
+    // button.id = "button-" + letter;
+
+    // };
+    // for (var i = 0; i < alphabet.length; i++) {
+    //     var buttonElement = createKeyboard(alphabet[i]);
+    //     document.getElementById("#keyboard").appendChild(buttonElement);
+    // }
+
+    // createKeyboard();
 // alert(answer);
